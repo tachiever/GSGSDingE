d083023a775476ca9dcd2fde7b497dcea910fb24
最爱海咪咪 <tachiever@126.com>
2018-04-10 09:24:28 +0800

    Initial Commit

---
+++ DingEr/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DingEr
+//
+//  Created by Tcy on 2018/4/10.
+//  Copyright © 2018年 Tcytachiever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
